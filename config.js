c0058ac10ff635976a8304fe2876ae029c054822
angadghatode <angad.ghatode@gmail.com>
2026-04-29 23:21:27 +0800

    inital setup

---
+++ config.js
@@ -0,0 +1,5 @@
+window.ENV = {
+  ADMIN_PASSWORD: "your_actual_password_here",
+  SUPABASE_URL: "https://okldowanguipkwvypshs.supabase.co",
+  SUPABASE_KEY: "sb_publishable_WGeBhzubq6QeOrK0e-XWww_aQrm3vLG"
+};
